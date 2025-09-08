f83c156f9bdd4d8388f5ac9946b5d1fad736e02d
Akshat Pawar <akshatpawar1@gmail.com>
2025-09-08 14:17:54 +0530

    updated backend

--- dashboard/src/components/Orders.js
+++ dashboard/src/components/Orders.js
@@ -1,17 +1,67 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// const Orders = () => {
+//   return (
+//     <div className="orders">
+//       <div className="no-orders">
+//         <p>You haven't placed any orders today</p>
+
+//         <Link to={"/"} className="btn">
+//           Get started
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Orders;
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Orders = () => {
-  return (
-    <div className="orders">
-      <div className="no-orders">
-        <p>You haven't placed any orders today</p>
+  const [allOrders, setAllOrders] = useState([]);
 
-        <Link to={"/"} className="btn">
-          Get started
-        </Link>
+  useEffect(() => {
+    axios.get("http://localhost:3002/allOrders").then((res) => {
+      setAllOrders(res.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h3 className="title">Orders ({allOrders.length})</h3>
+
+      <div className="order-table">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Instrument</th>
+              <th>Qty.</th>
+              <th>Price</th>
+              <th>Mode</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {allOrders.map((order, index) => {
+              const modeClass =
+                order.mode === "BUY" ? "profit" : "loss";
+
+              return (
+                <tr key={index}>
+                  <td>{order.name}</td>
+                  <td>{order.qty}</td>
+                  <td>{order.price.toFixed(2)}</td>
+                  <td className={modeClass}>{order.mode}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
-    </div>
+    </>
   );
 };
 
